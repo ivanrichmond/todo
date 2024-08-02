80744d0240f9db079d9dc4a3135db59176d0cc96
Ivan Richmond <irichmond@yahoo.com>
2024-08-01 20:48:12 -0700

    implement basic add todo functionality in AddTodo

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 import AppForm from '../designSystem/AppForm'
+import { TodoContext } from '../contexts/TodoProvider'
 
 const AddToDo = () => {
+    // states
+    const [task, setTask] = useState('')
+    const [dueDate, setDueDate] = useState('')
+
+    // contexts
+    const { addTodo } = useContext(TodoContext)
+
     return(
         <AppForm >
             <AppForm.Group inline>
-                <AppForm.Input label='New Task' placeholder='New Task' />
-                <AppForm.Input label='Due Date' placeholder='Due Date' type='date' />
-                <AppForm.Button icon='plus' />
+                <AppForm.Input 
+                label='New Task' 
+                placeholder='New Task' 
+                onChange = {(event) => setTask(event.target.value)}
+                />
+                <AppForm.Input 
+                label='Due Date' 
+                placeholder='Due Date' 
+                type='date' 
+                onChange = {(event) => setDueDate(event.target.value)}
+                />
+                <AppForm.Button icon='plus' onClick = {() => addTodo(task, dueDate)}/>
             </AppForm.Group>
         </AppForm>
     )
