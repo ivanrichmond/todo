a774a3116165595f72f100d906d608e314b33944
Ivan Richmond <irichmond@yahoo.com>
2024-08-02 11:29:33 -0700

    change status select to filter on change, not using search button

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,40 +1,23 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 
 import AppForm from '../designSystem/AppForm'
 import { TodoContext } from '../contexts/TodoProvider'
 
-//TODO: Change it so that selection automatically filters; eliminate Search button.
-
 const Search = () => {
     const { setFilterStatus } = useContext(TodoContext)
-    const [status, setStatus] = useState('all')
-    // TODO: use options and see if I can get AppForm.Select to work.
-    // AppForm.Select isn't working.  For some reason it complains that event.target.value is null.
-    const options = [
-        { key: 'a', text: 'All', value: 'all' },
-        { key: 'i', text: 'Incomplete', value: 'incomplete' },
-        { key: 'c', text: 'Complete', value: 'complete' },
-    ]
     return(
         <AppForm >
             <AppForm.Group inline >
                 <label htmlFor='status'>Status</label>
-                {/* <AppForm.Select 
-                defaultValue = 'All'
-                name='status'
-                onChange = {(event) => handleSelect(event)}
-                options={options}
-                placeholder='Status'
-                width={16}
-                /> */}
                 <select
-                onChange = {e => setStatus(e.target.value)}
+                className = 'ui selection dropdown'
+                name = 'status'
+                onChange = {e => setFilterStatus(e.target.value)}
                 >
                     <option value='all'>all</option>
                     <option value='complete'>complete</option>
                     <option value='incomplete'>incomplete</option>
                 </select>
-                <AppForm.Button icon='search' onClick = {() => setFilterStatus(status)}/>
             </AppForm.Group>
         </AppForm>
     )
