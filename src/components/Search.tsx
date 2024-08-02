cf8ed210b0ca45b5f5c1a0781f0228edfb8a9d0e
Ivan Richmond <irichmond@yahoo.com>
2024-08-01 23:36:26 -0700

    redo Search using basic HTML select, since SUI select is having problems

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 
 import AppForm from '../designSystem/AppForm'
+import { TodoContext } from '../contexts/TodoProvider'
 
 const Search = () => {
+    const { filter } = useContext(TodoContext)
+    const [status, setStatus] = useState('all')
+    // TODO: use options and see if I can get AppForm.Select to work.
+    // AppForm.Select isn't working.  For some reason it complains that event.target.value is null.
     const options = [
         { key: 'a', text: 'All', value: 'all' },
         { key: 'i', text: 'Incomplete', value: 'incomplete' },
@@ -12,14 +17,22 @@ const Search = () => {
         <AppForm >
             <AppForm.Group inline >
                 <label htmlFor='status'>Status</label>
-                <AppForm.Select 
+                {/* <AppForm.Select 
                 defaultValue = 'All'
                 name='status'
+                onChange = {(event) => handleSelect(event)}
                 options={options}
                 placeholder='Status'
                 width={16}
-                />
-                <AppForm.Button icon='search' />
+                /> */}
+                <select
+                onChange = {e => setStatus(e.target.value)}
+                >
+                    <option value='all'>all</option>
+                    <option value='complete'>complete</option>
+                    <option value='incomplete'>incomplete</option>
+                </select>
+                <AppForm.Button icon='search' onClick = {() => filter(status)}/>
             </AppForm.Group>
         </AppForm>
     )
