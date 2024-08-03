f90bd4778e675cbca266c2a6942e26287e830dbe
Ivan Richmond <irichmond@yahoo.com>
2024-08-02 18:39:19 -0700

    add className to AppSelect

--- src/designSystem/AppSelect.tsx
+++ src/designSystem/AppSelect.tsx
@@ -1,18 +1,25 @@
-export default function AppSelect({label, filter, options}:
-    {label: string, filter: (value: string) => {}, options: { value: string; text: string; }[]}){
+export type AppSelectProps = {
+    className?: string, 
+    label: string, 
+    filter: (value: string) => {}, 
+    options: { value: string; text: string; }[]
+}
+export default function AppSelect({className, label, filter, options}: AppSelectProps){
     const optionData = options.map((option,i) => {
         return <option key={i} value={option.value}>{option.text}</option>
     })
     return (
-        <>
+        <div className = {'ui form field'}>
             <label htmlFor='status'>{label}</label>
             <select
-            className = 'ui selection dropdown'
+            // Format like Semantic UI for consistency with SUI Form.
+            //.ui.form .field > .selection.dropdown
+            className = {`${className} selection dropdown`}
             name = {label}
             onChange = {e => filter(e.target.value)}
             >
                 {optionData}
             </select>
-        </>
+        </div>
     )
 }
