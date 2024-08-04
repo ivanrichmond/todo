4eff2f15cee6857386ec3c9a4ea8cdc5d0da6b6a
Ivan Richmond <irichmond@yahoo.com>
2024-08-03 18:55:52 -0700

    add SUI Popup link to comments for AppTooltip

--- src/designSystem/AppTooltip.tsx
+++ src/designSystem/AppTooltip.tsx
@@ -1,3 +1,4 @@
+// A wrapper around SUI's Popup: https://react.semantic-ui.com/modules/popup/ 
 import { Popup, PopupProps } from 'semantic-ui-react'
 
 export default function AppTooltip(props: PopupProps){
