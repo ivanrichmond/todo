b99ff860de589d572846201423d5489e1cd7a155
Ivan Richmond <irichmond@yahoo.com>
2024-08-03 17:45:58 -0700

    remove unused  from AppSelect

--- src/designSystem/AppSelect.tsx
+++ src/designSystem/AppSelect.tsx
@@ -10,17 +10,7 @@ export default function AppSelect({className, label, filter, options, tooltip}: 
     const optionData = options.map((option,i) => {
         return <option key={i} value={option.value}>{option.text}</option>
     })
-    const dropdown = (
-        <select
-        // Format like Semantic UI for consistency with SUI Form.
-        //.ui.form .field > .selection.dropdown
-        className = {`${className} selection dropdown`}
-        name = {label}
-        onChange = {e => filter(e.target.value)}
-        >
-            {optionData}
-        </select>
-    )
+    
     return (
         <div className = {'ui form field'}>
             <label htmlFor='status'>{label}</label>
