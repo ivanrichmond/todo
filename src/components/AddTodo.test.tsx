fc141eaf36d12f7aa0b21edf31409675b7676a7b
Ivan Richmond <irichmond@yahoo.com>
2024-08-04 17:40:21 -0700

    update unit tests for AddTodo

--- src/components/AddTodo.test.tsx
+++ src/components/AddTodo.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer'
 
 import AddTodo, { validateAddTodo } from './AddTodo';
@@ -24,9 +24,44 @@ describe('AddTodo', () => {
         expect(testText).toBeInTheDocument()
     })
 
+    // Functional test of form validation.
     test('validateAddTodo with valid parameters returns validate.isValid = true', () => {
         const validation = validateAddTodo('Write more unit tests', '8/6/2024')
         expect(validation.isValid).toBe(true)
+    })
+
+    // Test form submit.
+    test('Click Add button with New Task and Due Date filled in; both fields should get cleared', () => {
+        render(
+            <TodoProvider>
+                <NoticeProvider>
+                    <AddTodo />
+                </NoticeProvider>
+            </TodoProvider>
+        );
+
+        // First, set test data in each field.
+
+        // New Task is SUI, so the input is wrapped in a div, which has the data-testid.
+        // However the placeholder is actually on the input.
+        const newTask = screen.getByPlaceholderText(/new task/i)
+        fireEvent.change(newTask, {target: {value: 'test'}})
+        // byTestId works for Due Date, because I wrote the component! ;D
+        const dueDate = screen.getByTestId('due-date') 
+        // Because of the peculiarities of input type date, we need to enter it in YYYY-MM-DD format.
+        fireEvent.change(dueDate, {target: {value: '2024-08-05'}})
+
+        // Assert that the values are there to begin with.
+        expect(newTask).toHaveValue('test')
+        expect(dueDate).toHaveValue('2024-08-05')
+
+        // Click the Add button
+        const addButton = screen.getByTestId('add-button')
+        fireEvent.click(addButton)
+
+        // After Add is clicked, both fields should now be cleared.
+        expect(newTask).toHaveValue('')
+        expect(dueDate).toHaveValue('')
     })
 
     // Just make sure that the component starts out rendered as expected.
